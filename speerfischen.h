e89b7944206761003a685d926325ddbf781eb549
Matthias Maile <matthias.maile@udo.edu>
2021-01-29 19:26:07 +0100

    struct and function prototypes

--- speerfischen.h
+++ speerfischen.h
@@ -1,2 +1,11 @@
 /* speerfischen.h start */
+
+typedef struct coordinate
+{
+	double x,y;
+} coordinate;
+
+coordinate *calc_path_given_y(coordinate *origin, double y_end, double angle);
+coordinate *calc_path_given_x(coordinate *origin, double x_end, double angle);
+
 /* speerfischen.h end */
