f50a83f754d9fe0d34bf78b8af86bcd6c426db73
Matthias Maile <matthias.maile@udo.edu>
2021-02-01 22:11:06 +0100

    better output and controlling result

--- plot.py
+++ plot.py
@@ -12,10 +12,18 @@ y2 = df['y2']
 
 res = np.abs(y2 - 2).argmin()
 best_angle = a[res]
+angle_arrive = df['b'][res]
 
 print(f'The best angle was found to be {best_angle}.')
+print(f'The light arrives at the fishers eye at angle {angle_arrive}.')
 
-
+# kontrollrechnung
+alpha = best_angle
+dist = 1
+depth = 1.5
+n_wt = 1.33
+h = (dist - np.sin(alpha) * depth) / np.tan(np.arcsin(n_wt * np.sin(alpha)))
+print(f'h = {h}')
 
 plt.plot(a, np.abs(y2 - 2))
 plt.xlabel(r'$\alpha$')
